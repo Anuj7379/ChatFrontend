95da4574ae7459a8b0e22c570e7edc457c0b042b
anuj sharma <as9737856@gmail.com>
2025-06-15 23:13:50 +0530

    fix: prepend HOST to all API routes

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,30 +1,25 @@
 export const HOST = import.meta.env.VITE_SERVER_URL;
-export const SOCKET_HOST = import.meta.env.VITE_SERVER_URL;
+export const SOCKET_HOST = import.meta.env.VITE_SOCKET_URL;
 
-export const AUTH_ROUTES = "api/auth";
-export const LOGIN_ROUTE = `${AUTH_ROUTES}/login`;
-export const SIGNUP_ROUTE = `${AUTH_ROUTES}/signup`;
-export const GET_USERINFO_ROUTE = `${AUTH_ROUTES}/userinfo`;
-export const LOGOUT_ROUTE = `${AUTH_ROUTES}/logout`;
-export const UPDATE_PROFLE_ROUTE = `${AUTH_ROUTES}/update-profile`;
-export const ADD_PROFILE_IMAGE_ROUTE = `${AUTH_ROUTES}/add-profile-image`;
-export const REMOVE_PROFILE_IMAGE_ROUTE = `${AUTH_ROUTES}/remove-profile-image`;
+// Auth
+export const LOGIN_ROUTE = `${HOST}/api/auth/login`;
+export const SIGNUP_ROUTE = `${HOST}/api/auth/signup`;
+export const GET_USERINFO_ROUTE = `${HOST}/api/auth/userinfo`;
+export const LOGOUT_ROUTE = `${HOST}/api/auth/logout`;
+export const UPDATE_PROFLE_ROUTE = `${HOST}/api/auth/update-profile`;
+export const ADD_PROFILE_IMAGE_ROUTE = `${HOST}/api/auth/add-profile-image`;
+export const REMOVE_PROFILE_IMAGE_ROUTE = `${HOST}/api/auth/remove-profile-image`;
 
-export const MESSAGES_ROUTES = "/api/messages";
-export const FETCH_ALL_MESSAGES_ROUTE = `${MESSAGES_ROUTES}/get-messages`;
-export const UPLOAD_FILE = `${MESSAGES_ROUTES}/upload-file`;
+// Messages
+export const FETCH_ALL_MESSAGES_ROUTE = `${HOST}/api/messages/get-messages`;
+export const UPLOAD_FILE = `${HOST}/api/messages/upload-file`;
 
-export const CHANNEL_ROUTES = "/api/channel";
-export const CREATE_CHANNEL = `${CHANNEL_ROUTES}/create-channel`;
-export const GET_USER_CHANNELS = `${CHANNEL_ROUTES}/get-user-channels`;
-export const GET_CHANNEL_MESSAGES = `${CHANNEL_ROUTES}/get-channel-messages`;
+// Channels
+export const CREATE_CHANNEL = `${HOST}/api/channel/create-channel`;
+export const GET_USER_CHANNELS = `${HOST}/api/channel/get-user-channels`;
+export const GET_CHANNEL_MESSAGES = `${HOST}/api/channel/get-channel-messages`;
 
-export const CONTACTS_ROTUES = "/api/contacts";
-export const SEARCH_CONTACTS_ROUTES = `${CONTACTS_ROTUES}/search`;
-export const GET_CONTACTS_WITH_MESSAGES_ROUTE = `${CONTACTS_ROTUES}/get-contacts-for-list`;
-export const GET_ALL_CONTACTS = `${CONTACTS_ROTUES}/all-contacts`;
-
-export const MESSAGE_TYPES = {
-  TEXT: "text",
-  FILE: "file",
-};
+// Contacts
+export const SEARCH_CONTACTS_ROUTES = `${HOST}/api/contacts/search`;
+export const GET_CONTACTS_WITH_MESSAGES_ROUTE = `${HOST}/api/contacts/get-contacts-for-list`;
+export const GET_ALL_CONTACTS = `${HOST}/api/contacts/all-contacts`;
